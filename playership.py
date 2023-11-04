6525e60aa61bbdd227c38bfe6268200cdad5b06c
jorkroc <jrkane42@gmail.com>
2023-11-04 16:58:57 -0400

    territory start

--- playership.py
+++ playership.py
@@ -1,5 +1,17 @@
 import ship
+from enum import Enum
+
+class Territory(Enum):
+    SAFE = 0
+    ROYAL = 1
+    RAMMER = 2
+    GRAPE = 3
+    JUGGERNAUT = 4
 
 class PlayerShip(ship.Ship):
     def __init__(self, xpos, ypos):
         super().__init__("red", 40, 40, xpos, ypos)
+        self.territory = Territory.SAFE
+    
+    def updateTerr():
+        self.territory= Territory.SAFE
