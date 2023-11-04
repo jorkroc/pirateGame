41a3c8da60d87eefeb4de481e609b02a93c74791
Aakash Karlekar <ak@macbook-pro.resource.campus.njit.edu>
2023-11-04 14:31:20 -0400

    Add additional methods to Island Class

--- Island.py
+++ Island.py
@@ -1,6 +1,18 @@
 class Island:
 
-    def __init__(self, position, color):
+    def __init__(self, position, color, treasure):
         self.position = position
         self.color = color
+        self.treasure = treasure
+
+    def getPosition(self):
+        return self.position
+
+    def setPosition(self, newPosition):
+        self.position = newPosition
+
+    def getTreasure(self):
+        return self.treasure
     
+    def setTreasure(self, newTreasure):
+        self.treasure = newTreasure
