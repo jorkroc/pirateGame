1487deb5103cdfe319506a9c2840bbfd65889a1b
jorkroc <jrkane42@gmail.com>
2023-11-04 15:17:52 -0400

    change island to sprited 1

--- Island.py
+++ Island.py
@@ -1,6 +1,12 @@
-class Island:
+import pygame
+
+class Island(pygame.sprite.Sprite):
 
     def __init__(self, position, color, treasure):
+        super().__init__()
+        self.image = pygame.Surface([80,80])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
         self.position = position
         self.color = color
         self.treasure = treasure
